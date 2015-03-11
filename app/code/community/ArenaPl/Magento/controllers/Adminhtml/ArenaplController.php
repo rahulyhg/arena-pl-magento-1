4015a1e5afbf1840c3994abefff6c0a71151fecc
Dawid Spiechowicz <spiechu@gmail.com>
2015-03-11 15:30:32 +0100

    working blocks

--- app/code/community/ArenaPl/Magento/controllers/Adminhtml/ArenaplController.php
+++ app/code/community/ArenaPl/Magento/controllers/Adminhtml/ArenaplController.php
@@ -4,15 +4,56 @@ class ArenaPl_Magento_Adminhtml_ArenaplController extends Mage_Adminhtml_Control
 {
     public function ordersAction()
     {
+        $this->setActiveArenaMenu();
+
+        $this->setActiveBlock('arenapl_orders', 'arenapl/orders.phtml');
+
+        $this->renderLayout();
+    }
+
+    public function accountAction()
+    {
+        $this->setActiveArenaMenu();
+
+        $this->setActiveBlock('arenapl_account', 'arenapl/account.phtml');
+
+        $this->renderLayout();
+    }
+
+    public function categoriesAction()
+    {
+        $this->setActiveArenaMenu();
+
+        $this->setActiveBlock('arenapl_categories', 'arenapl/categories.phtml');
+
+        $this->renderLayout();
+    }
+
+    public function helpAction()
+    {
+        $this->setActiveArenaMenu();
+
+        $this->setActiveBlock('arenapl_help', 'arenapl/help.phtml');
+
+        $this->renderLayout();
+    }
+
+    protected function setActiveArenaMenu()
+    {
         $layout = $this->loadLayout();
         $layout->_setActiveMenu('arenapl_magento');
+    }
 
+    protected function setActiveBlock($blockName, $templateFile)
+    {
         $block = $this->getLayout()->createBlock(
-                'Mage_Core_Block_Template', 'arenapl_orders', ['template' => 'arenapl/orders.phtml']
+            'Mage_Core_Block_Template',
+            $blockName,
+            [
+                'template' => $templateFile,
+            ]
         );
 
         $this->getLayout()->getBlock('content')->append($block);
-
-        $this->renderLayout();
     }
 }
