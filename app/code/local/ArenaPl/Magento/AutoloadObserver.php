b798dc7c6cd8c8d9e1d3f5a719ee85893227e5a6
Dawid Spiechowicz <spiechu@gmail.com>
2015-03-11 13:47:17 +0100

    autoloaders

---
+++ app/code/local/ArenaPl/Magento/AutoloadObserver.php
@@ -0,0 +1,20 @@
+<?php
+
+class ArenaPl_Magento_AutoloadObserver
+{
+    /**
+     * @var bool
+     */
+    protected $autoloaderInjected = false;
+
+    /**
+     * @param Varien_Event_Observer $observer
+     */
+    public function injectComposerAutoloader(Varien_Event_Observer $observer)
+    {
+        if (!$this->autoloaderInjected) {
+            $autoloadInjector = new ArenaPl_Magento_AutoloadInjector();
+            $this->autoloaderInjected = $autoloadInjector->injectComposerAutoloader();
+        }
+    }
+}
