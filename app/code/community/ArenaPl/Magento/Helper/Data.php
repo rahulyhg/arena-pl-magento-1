80b9db21ff16b60029fadc64ee2e64b111286d38
Dawid Spiechowicz <spiechu@gmail.com>
2015-03-13 12:24:07 +0100

    client SDK factory

--- app/code/community/ArenaPl/Magento/Helper/Data.php
+++ app/code/community/ArenaPl/Magento/Helper/Data.php
@@ -1,5 +1,38 @@
 <?php
 
+use ArenaPl\Client;
+
 class ArenaPl_Magento_Helper_Data extends Mage_Core_Helper_Abstract
 {
+    /**
+     * @var Client
+     */
+    protected $client;
+
+    /**
+     * @return Client
+     */
+    public function getClient()
+    {
+        if (!$this->client) {
+            $this->client = $this->createClient();
+        }
+
+        return $this->client;
+    }
+
+    /**
+     * @return Client
+     */
+    protected function createClient()
+    {
+        /* @var $accountModel ArenaPl_Magento_Model_Account */
+        $accountModel = Mage::getModel('arenapl_magento/account');
+
+        return new Client(
+            $accountModel->getSubdomain(),
+            $accountModel->getToken(),
+            Mage::getIsDeveloperMode()
+        );
+    }
 }
