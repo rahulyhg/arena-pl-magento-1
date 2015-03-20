cfc8ec2354f04417bcd6c3eb8dcf969e52b71da1
Dawid Spiechowicz <spiechu@gmail.com>
2015-03-20 09:20:41 +0100

    observer

---
+++ app/code/community/ArenaPl/Magento/Observer/Product.php
@@ -0,0 +1,11 @@
+<?php
+
+class ArenaPl_Magento_Observer_Product
+{
+    /**
+     * @param Varien_Event_Observer $observer
+     */
+    public function afterProductSave(Varien_Event_Observer $observer)
+    {
+    }
+}
